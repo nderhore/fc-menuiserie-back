6f39d57400fbbe7cb85252d95ff1caac19f6850c
nderhore <nathan@derhore.fr>
2023-10-12 20:51:36 +0200

    add horaire in import sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,1 +1,7 @@
 INSERT INTO menuiserie(nom) VALUES ('Menuiserie de Julien');
+INSERT INTO horaire(heure_apres_midi,heure_matin, jour) VALUES ('14:00-19:00','8:00-12:00', 'LUNDI'),
+                                                                               ('13:00-16:00','9:00-12:00', 'MARDI'),
+                                                                               ('14:00-17:00','11:00-12:00', 'MERCREDI'),
+                                                                               ('15:00-19:00','10:0030-11:0000', 'JEUDI'),
+                                                                               ('16:00-18:00','10:00-12:00', 'VENDREDI'),
+                                                                               ('','8:00-12:00', 'SAMEDI');
