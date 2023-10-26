a47a8e69bdbeef2c8e1a402b56ba8b7160db6f00
nderhore <nathan@derhore.fr>
2023-10-26 20:37:06 +0200

    add auth ws

---
+++ src/main/java/fr/studi/menuiserie/ws/AuthWs.java
@@ -0,0 +1,13 @@
+package fr.studi.menuiserie.ws;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(ApiRegistration.REST_LOGIN)
+@CrossOrigin
+public class AuthWs {
+
+
+}
