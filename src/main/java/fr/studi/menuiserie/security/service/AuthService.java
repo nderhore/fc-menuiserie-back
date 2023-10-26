2b9f8f1633f00942bd00a409a9eb4d2e22671fc2
nderhore <nathan@derhore.fr>
2023-10-26 20:38:03 +0200

    implements auth service

---
+++ src/main/java/fr/studi/menuiserie/security/service/AuthService.java
@@ -0,0 +1,17 @@
+package fr.studi.menuiserie.security.service;
+
+import fr.studi.menuiserie.security.payload.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+}
